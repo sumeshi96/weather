31143510e0d4a710cc877f68287ecda28e93b44e
sumeshi96 <yumapc96@gmail.com>
2022-04-09 16:43:45 +0900

    add get wether data

--- weather.py
+++ weather.py
@@ -1,6 +1,49 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = requests.get("https://tenki.jp/forecast/1/4/2300/1202/")
 
-soup = BeautifulSoup(url.text, "lxml")
+def main(URL):
+    url = requests.get(URL)
+
+    soup = BeautifulSoup(url.text, "lxml")
+
+    soup_today = soup.select('.today-weather')[0]
+    soup_tomorrow = soup.select('.tomorrow-weather')[0]
+    output(soup_today)
+    output(soup_tomorrow)
+
+def output(soup):
+    #天気
+    weather_telop = soup.find("p",class_="weather-telop").get_text()
+
+    #最高気温
+    high_temp = soup.find("dd",class_="high-temp").get_text()
+
+    #最低気温
+    low_temp = soup.find("dd",class_="low-temp").get_text()
+
+    #降水確率
+    rain_probability = soup.select('.rain-probability > td')
+    every_six = {}
+    for i in range(4):
+        time_from = 0+6*i
+        time_to = 6+6*i
+        time = "{:02}-{:02}".format(time_from,time_to)
+        every_six[time] = rain_probability[i].text.strip()
+
+    print(
+        "天気："+ str(weather_telop) +"\n"
+        "最高気温：" + str(high_temp) + "\n"
+        "最低気温：" + str(low_temp) + "\n"
+        "降水確率['00-06']：" + str(every_six['00-06']) + "\n"
+        "降水確率['06-12']：" + str(every_six['06-12']) + "\n"
+        "降水確率['12-18']：" + str(every_six['12-18']) + "\n"
+        "降水確率['18-24']：" + str(every_six['18-24']) + "\n"
+    )  
+
+
+
+if __name__ == '__main__':
+        URL="https://tenki.jp/forecast/1/4/2300/1202/"
+        main(URL)
+
